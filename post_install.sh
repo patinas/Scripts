5701daa43e042a9d823f85be04b4cfa48541f7a8
patinas <andreas.patinas@gmail.com>
2022-01-20 08:57:49 +0100

    Add files via upload

---
+++ post_install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+while true; do ping -c1 www.google.com > /dev/null && break; done
+
+sudo curl -s https://install.zerotier.com | sudo bash
+sudo zerotier-cli join 9f77fc393e977c16
+
+sudo apt update && sudo apt upgrade -y
+
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
+
+sudo apt update && sudo apt upgrade -y
+
+exit
