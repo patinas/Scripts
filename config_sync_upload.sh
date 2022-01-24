5aa0a8ac9c8d067924417c2349f64d096731162a
patinas <andreas.patinas@gmail.com>
2022-01-24 14:28:35 +0100

    Update config_sync_upload.sh

--- config_sync_upload.sh
+++ config_sync_upload.sh
@@ -1,39 +1,17 @@
 #!/bin/sh
 
-mkdir ~/Scripts/
 mkdir ~/Sync/
+mkdir ~/Scripts/
 
 while true; do ping -c1 www.google.com > /dev/null && break; done
-
+sudo apt update && sudo apt upgrade -y
+sudo apt install dconf* -y
 sudo apt update && sudo apt upgrade -y
 
-## Remember to connect Google Drive as remote
-# sudo apt install rclone -y
-
-dconf dump /org/cinnamon/ > cinnamon.dconf
-dconf dump /org/nemo/ > nemo.dconf
-dconf dump /org/gtk/ > gtk.dconf
-dconf dump /org/gnome/ > gnome.dconf
-
-sudo mv cinnamon.dconf ~
-sudo mv nemo.dconf ~
-sudo mv gtk.dconf ~
-sudo mv gnome.dconf ~
-
-## Backup to google drive
-sudo rsync -aP ~/cinnamon.dconf  ~/Sync/
-sudo rsync -aP ~/nemo.dconf  ~/Sync/
-sudo rsync -aP ~/gtk.dconf  ~/Sync/
-sudo rsync -aP ~/gnome.dconf  ~/Sync/
-
-## Backup to google drive
-# sudo rsync -aP --exclude 'google-chrome' ~/.config/ ~/Sync/.config/
-# sudo rsync -aP ~/.cinnamon/ ~/Sync/.cinnamon/
+## Backup settings
+dconf dump / > cinnamon_desktop
+mv cinnamon_desktop ~/Sync/
 
 ## Sync with google drive
 sudo rclone sync -P ~/Sync/ drive:/Sync/
 sudo rclone sync -P ~/Scripts/ drive:/Scripts/
-
-sudo apt update && sudo apt upgrade -y
-
-exit
