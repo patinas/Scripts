cfebcb4aada088ba2ec18f9b966ced92de0a3f88
patinas <andreas.patinas@gmail.com>
2022-01-24 08:55:22 +0100

    Update config_sync_down.sh

--- config_sync_down.sh
+++ config_sync_down.sh
@@ -8,15 +8,15 @@ while true; do ping -c1 www.google.com > /dev/null && break; done
 sudo apt update && sudo apt upgrade -y
 
 ## Remember to connect Google Drive as remote
-sudo apt install rclone -y
+# sudo apt install rclone -y
 
 ## Sync with google drive
-sudo rclone sync -P ~/Sync/ drive:/Sync/
-sudo rclone sync -P ~/Scripts/ drive:/Scripts/
+sudo rclone sync -P drive:/Sync/ ~/Sync/
+sudo rclone sync -P drive:/Scripts/ ~/Scripts/
 
 ## Download new config
-sudo rm -rf ~/.config/
-sudo rm -rf ~/.cinnamon/
+# sudo rm -rf ~/.config/
+# sudo rm -rf ~/.cinnamon/
 sudo rsync -aPv ~/Sync/.config/ ~/.config/
 sudo rsync -aPv ~/Sync/.cinnamon/ ~/.cinnamon/
 
