77f38f3f37a3cf6bd281cb73e258798a7c388d32
patinas <andreas.patinas@gmail.com>
2022-02-25 08:47:23 +0100

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+dpkg --configure -a &&
+apt update &&
+apt upgrade -y &&
+apt install -f &&
+apt clean &&
+apt autoclean &&
+apt autoremove -y
+sudo chmod +x /home/user/post_install_linux/*
