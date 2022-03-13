5f78b37da87803e015d611fc4138e74b8f4edbfb
patinas <andreas.patinas@gmail.com>
2022-03-13 23:50:28 +0100

    Update post_install_minimal.sh

--- post_install_minimal.sh
+++ post_install_minimal.sh
@@ -5,16 +5,11 @@ while true; do ping -c1 www.google.com > /dev/null && break; done
 sudo apt update && sudo apt upgrade -y
 
 sudo apt install curl -y
-sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb -y
 
-sudo apt install peek -y
-sudo apt install remmina -y
-sudo apt install variety -y
 sudo curl -s https://install.zerotier.com | sudo bash
 sudo zerotier-cli join 9f77fc393e977c16
 
-sudo chmod +x post_install_minimal.sh
+sudo chmod +x *.sh
 
 curl https://getmic.ro | bash
 sudo chown root:root micro
