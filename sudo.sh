fb0126045ea63ab49e9a87e312322b9adc480f57
patinas <andreas.patinas@gmail.com>
2022-03-20 14:59:46 +0100

    Update sudo.sh

--- sudo.sh
+++ sudo.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+dpkg --configure -a &&
+apt update &&
+apt upgrade -y &&
+apt install -f &&
+apt clean &&
+apt autoclean &&
+apt autoremove -y
+
+sudo chmod +x *.sh
+
 sudo echo 'user ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers
