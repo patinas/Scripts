1b48a3d98da9a5f236357a340baec4ac95350df9
patinas <andreas.patinas@gmail.com>
2022-03-22 13:54:01 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo dpkg --configure -a &&
 sudo apt update &&
+sudo apt install libnotify-bin -y 
 sudo apt upgrade -y &&
 sudo apt install -f &&
 sudo apt clean &&
@@ -8,9 +9,10 @@ sudo apt autoclean &&
 sudo apt autoremove -y
 sudo flatpak update -y
 
-
 sudo chmod +x *.sh
 
 cat <<EOF | crontab -
 */30 * * * * /root/post_install_linux/update.sh
 EOF
+
+notify-send "Update done!"
