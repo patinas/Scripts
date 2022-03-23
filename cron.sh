dd02e8a897dcea5f6ba222e8a7391229fe4fb027
patinas <andreas.patinas@gmail.com>
2022-03-23 17:19:58 +0100

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 
-(crontab -l 2>/dev/null; echo "*/30 * * * * /bin/sh /root/post_install_linux/update.sh >> /root/update.log 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "*/30 * * * * /usr/bin/sh /root/post_install_linux/update.sh") | crontab -
