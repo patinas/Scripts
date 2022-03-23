a47c57e0782cb52e1a976727fe5ae4fd8d529cb4
patinas <andreas.patinas@gmail.com>
2022-03-23 01:49:26 +0100

    Delete joomla_docker.sh

--- joomla_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-while true; do ping -c1 www.google.com > /dev/null && break; done
-sudo apt update && sudo apt upgrade -y
-sudo chmod +x joomla_docker.sh
-sudo docker pull joomla
-sudo docker pull mysql
-sudo docker run --name joomla_db -d -e MYSQL_ROOT_PASSWORD=test mysql
-sudo docker run --name joomla_website --link joomla_db:mysql -d -p 8888:80 joomla
