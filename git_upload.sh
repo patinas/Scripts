a38b28b7fab72548b643a1c0c3836e9de8935dba
patinas <andreas.patinas@gmail.com>
2022-03-23 09:35:40 +0100

    Update git_upload.sh

--- git_upload.sh
+++ git_upload.sh
@@ -1,7 +1,20 @@
-#!/bin/sh
-chmod +x *.sh
+#!/bin/bash
+sudo chmod +x *.sh
+gstatus=`git status --porcelain`
 
-## Remember to git clone with SSH before running this script
-git add .
-git commit -m "sync"
-git push
+if [ ${#gstatus} -ne 0 ]
+then
+
+    git add --all
+    git commit -m "$gstatus"
+
+	git pull
+    git push
+
+fi
+
+cat <<EOF | crontab -
+*/30 * * * * /root/post_install_linux/update.sh
+*/5 * * * * /root/config/sync.sh
+*/3 * * * * /root/config/upload.sh
+EOF
