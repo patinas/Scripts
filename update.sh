6847625556809b4f0216a5befb3d25bf5090b63f
root <root@pop-os.localdomain>
2022-03-23 15:08:00 +0100

    M update.sh ?? cron.sh

--- update.sh
+++ update.sh
@@ -12,5 +12,3 @@ sudo apt autoremove -y
 sudo flatpak update -y
 
 sudo chmod +x *.sh
-
-(crontab -l 2>/dev/null; echo "*/30 * * * * /bin/sh /root/post_install_linux/update.sh >> /root/update.log 2>&1") | crontab -
