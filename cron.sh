79c9d92a6adf0d4d44f1b2cea026a11713e7e817
patinas <andreas.patinas@gmail.com>
2022-03-23 15:10:43 +0100

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-while true; do ping -c1 www.google.com > /dev/null && break; done
-
 
 (crontab -l 2>/dev/null; echo "*/30 * * * * /bin/sh /root/post_install_linux/update.sh >> /root/update.log 2>&1") | crontab -
