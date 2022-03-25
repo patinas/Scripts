d786a9297250f3c480acfd51c1182bea42c2e695
root <root@pop-os.localdomain>
2022-03-25 01:52:51 +0100

    M post_install_full.sh

--- post_install_full.sh
+++ post_install_full.sh
@@ -14,10 +14,8 @@ sudo apt install openssh-server -y
 sudo apt install peek -y
 sudo apt install variety -y
 sudo apt install flatpak -y
-flatpak install https://flatpak.elementary.io/repo/appstream/com.github.phase1geo.annotator.flatpakref
+flatpak install https://flatpak.elementary.io/repo/appstream/com.github.phase1geo.annotator.flatpakref -y
 sudo apt install snapd -y
-sudo snap install ncspot
-sudo apt install libncursesw5-dev libdbus-1-dev libpulse-dev libssl-dev libxcb1-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev -y
 
 
 sudo curl -s https://install.zerotier.com | sudo bash
