bbe3e60cc45c83a94dd64fe1ab390fafccaa6b9f
root <root@pop-os.localdomain>
2022-03-25 20:37:11 +0100

    M spotifyd.sh

--- spotifyd.sh
+++ spotifyd.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-cd /root/
+cd /home/user
 sudo apt update
 sudo apt install rustc cargo libasound2-dev libssl-dev pkg-config
 git clone https://github.com/Spotifyd/spotifyd.git
 cd spotifyd
 cargo build --release
 cp /root/other_scripts/spotifyd/contrib/spotifyd.service /etc/systemd/user/
-systemctl --user start spotifyd.service
-systemctl --user enable spotifyd.service
+sudo systemctl --user start spotifyd.service
+sudo systemctl --user enable spotifyd.service
 ./target/release/spotifyd
