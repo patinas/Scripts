28b40046a29da548075af38d84f863d425be8a3a
patinas <andreas.patinas@gmail.com>
2022-03-25 20:52:41 +0100

    Update spotifyd.sh

--- spotifyd.sh
+++ spotifyd.sh
@@ -5,7 +5,8 @@ sudo apt install rustc cargo libasound2-dev libssl-dev pkg-config
 git clone https://github.com/Spotifyd/spotifyd.git
 cd spotifyd
 cargo build --release
-cp /root/other_scripts/spotifyd/contrib/spotifyd.service /etc/systemd/user/
+sudo cp ~/spotifyd/contrib/spotifyd.service /etc/systemd/user/
 sudo systemctl --user start spotifyd.service
 sudo systemctl --user enable spotifyd.service
+cd ~/spotifyd
 ./target/release/spotifyd
