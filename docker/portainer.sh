73b3e063d42f607aa9e8f7b8fd2a4f314008af07
patinas <andreas.patinas@gmail.com>
2022-04-01 00:38:40 +0200

    Add files via upload

---
+++ docker/portainer.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+while true; do ping -c1 www.google.com > /dev/null && break; done
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install curl -y
+
+sudo apt install docker.io docker-compose -y
+sudo systemctl enable docker
+sudo systemctl start docker
+
+sudo docker run -d \
+--name="portainer" \
+--restart on-failure \
+-p 9000:9000 \
+-p 8000:8000 \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v portainer_data:/data \
+portainer/portainer-ce:latest
+
+sudo curl -s https://install.zerotier.com | sudo bash
+sudo zerotier-cli join 9f77fc393e977c16
+
+sudo chmod +x portainer.sh
+
+echo Done
+
+exit
