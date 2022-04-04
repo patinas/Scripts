5cb66b7a77d5ca43df94ee233eb2af9b8aa41859
patinas <andreas.patinas@gmail.com>
2022-04-04 22:17:28 +0200

    Update SyncThing.sh

--- docker/SyncThing.sh
+++ docker/SyncThing.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
 
+while true; do ping -c1 www.google.com > /dev/null && break; done
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install curl -y
+
+sudo apt install docker.io docker-compose -y
+sudo systemctl enable docker
+sudo systemctl start docker
+
+sudo docker run -d \
+--name="portainer" \
+--restart on-failure \
+-p 9000:9000 \
+-p 8000:8000 \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v portainer_data:/data \
+portainer/portainer-ce:latest
+
+sudo curl -s https://install.zerotier.com | sudo bash
+sudo zerotier-cli join 9f77fc393e977c16
+
+sudo chmod +x *.sh
+
 sudo docker run -d \
   --name=syncthing \
   --hostname=syncthing `#optional` \
@@ -14,3 +38,7 @@ sudo docker run -d \
   -v /root/syncthing/data:/data \
   --restart unless-stopped \
   lscr.io/linuxserver/syncthing
+
+echo Done
+
+exit
