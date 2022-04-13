fef18fe7d66e26da252573f3639fb81a82c93370
patinas <andreas.patinas@gmail.com>
2022-04-13 22:05:11 +0200

    Update gui_minimal.sh

--- gui_minimal.sh
+++ gui_minimal.sh
@@ -18,6 +18,13 @@ sudo chown root:root micro
 sudo mv micro /usr/local/bin
 sudo apt install xclip -y
 
+sudo apt install apt-transport-https curl -y
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg -y
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list -y
+sudo apt update
+sudo apt install brave-browser
+
+
 echo Done
 
 sudo reboot
