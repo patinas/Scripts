e9be2f1b3e6047ec2a46668f8882af797bc46a4f
patinas <andreas.patinas@gmail.com>
2022-04-13 21:57:00 +0200

    Update gui_minimal.sh

--- gui_minimal.sh
+++ gui_minimal.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
-sudo apt update
+
+while true; do ping -c1 www.google.com > /dev/null && break; done
+
+sudo apt update && sudo apt upgrade -y
+
 sudo apt install lxqt sddm -y
 sudo apt install lxqt openbox -y
+
+sudo apt install curl -y
+
+sudo curl -s https://install.zerotier.com | sudo bash
+sudo zerotier-cli join 9f77fc393e977c16
+
+sudo chmod +x *.sh
+
+curl https://getmic.ro | bash
+sudo chown root:root micro
+sudo mv micro /usr/local/bin
+sudo apt install xclip -y
+
+echo Done
+
 sudo reboot
