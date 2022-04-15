c7eb18a33ed7d3bacc6641da356f48bf7cd8a7fd
patinas <andreas.patinas@gmail.com>
2022-04-15 18:33:34 +0200

    Update rclone.sh

--- rclone.sh
+++ rclone.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
 
+while true; do ping -c1 www.google.com > /dev/null && break; done
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install rclone -y
+
+rclone config
+
+(crontab -l ; echo "* * * * * rclone sync /home/user/Downloads/ gdrive:/Sync/ >/dev/null 2>&1")| crontab -
+
+chmod +x *.sh
