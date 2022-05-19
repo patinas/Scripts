bec0ccbe8daf56f23dd13f70c1e57391262ebf3f
patinas <andreas.patinas@gmail.com>
2022-05-19 08:05:12 +0000

    Update polybar.sh

--- polybar.sh
+++ polybar.sh
@@ -1,4 +1,5 @@
-!#/bin/sh
+#!/bin/sh
+
 sudo apt update
 sudo apt install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python3-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev -y
 sudo apt install libxcb-composite0-dev -y
