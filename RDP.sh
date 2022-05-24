8753dc8af359b8ef6f92e52ea0a9dec879abf5ae
patinas <andreas.patinas@gmail.com>
2022-05-24 12:37:50 +0200

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+while true; do ping -c1 www.google.com > /dev/null && break; done
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install curl -y
+
+sudo curl -s https://install.zerotier.com | sudo bash
+sudo zerotier-cli join 9f77fc393e977c16
+
 adduser rdpuser
 usermod -aG sudo rdpuser
 apt-get update
