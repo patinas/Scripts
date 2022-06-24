ea9b4760919f91962493b0af916ad13d29fdcccc
patinas <andreas.patinas@gmail.com>
2022-06-24 10:31:34 +0200

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 (crontab -l ; echo "*/30 * * * * /root/scripts/update.sh >/dev/null 2>&1")| crontab -
-(crontab -l ; echo "*/30 * * * * /root/scripts/upload.sh >/dev/null 2>&1")| crontab -
-(crontab -l ; echo "@reboot /root/scripts/download.sh >/dev/null 2>&1")| crontab -
+
 
 chmod +x *.sh
