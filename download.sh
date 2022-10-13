397fc1a9adf7377905d4b07bf729ec0b1d372ad7
patinas <andreas.patinas@gmail.com>
2022-10-13 10:23:44 +0200

    Delete download.sh

--- download.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd /root/
-rm -r /root/scripts
-git clone git@github.com:patinas/scripts.git
-cp /root/scripts/rc.lua /home/user/.config/awesome/
-cp -r /root/scripts/alacritty /home/user/.config/ 
