3cf92aed4206b28c9c2bf0ea42bbb94b9c581c7e
patinas <andreas.patinas@gmail.com>
2022-10-13 11:15:31 +0200

    Update block.sh

--- block.sh
+++ block.sh
@@ -14,39 +14,9 @@ EOF
 apt update
 apt install docker.io -y
 
-tee pihole.sh <<EOF
-#!/bin/bash
-
-docker run -d \
-    --name pihole \
-    -p 53:53/tcp -p 53:53/udp \
-    -p 80:80 \
-    -p 443:443 \
-    -p 8080:8080 \
-    -e TZ="Europe/Copenhagen" \
-    -v "$(pwd)/etc-pihole/:/etc/pihole/" \
-    -v "$(pwd)/etc-dnsmasq.d/:/etc/dnsmasq.d/" \
-    --dns=127.0.0.1 --dns=1.1.1.1 \
-    --restart=unless-stopped \
-    thenetworkchuck/networkchuck_pihole
-
-printf 'Starting up pihole container '
-for i in $(seq 1 20); do
-    if [ "$(docker inspect -f "{{.State.Health.Status}}" pihole)" == "healthy" ] ; then
-        printf ' OK'
-        echo -e "\n$(docker logs pihole 2> /dev/null | grep 'password:') for your pi-hole: https://${IP}/admin/"
-        exit 0
-    else
-        sleep 3
-        printf '.'
-    fi
-
-    if [ $i -eq 20 ] ; then
-        echo -e "\nTimed out waiting for Pi-hole start, consult check your container logs for more info (\`docker logs pihole\`)"
-        exit 1
-    fi
-done;
-EOF
+apt install git -y
+git clone https://github.com/patinas/pihole
+cd pihole
 
 chmod u+x pihole.sh
 ./pihole.sh
