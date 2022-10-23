6eb37181921bfc197c9fbf6d863aa47e12167dc7
patinas <andreas.patinas@gmail.com>
2022-10-23 17:31:22 +0200

    Update kasm.sh

--- kasm.sh
+++ kasm.sh
@@ -6,7 +6,6 @@ sudo mkswap /mnt/4GiB.swap
 sudo swapon /mnt/4GiB.swap
 echo '/mnt/4GiB.swap swap swap defaults 0 0' | sudo tee -a /etc/fstab
 
-cd /tmp
 curl -O https://kasm-static-content.s3.amazonaws.com/kasm_release_1.11.0.18142e.tar.gz
 tar -xf kasm_release*.tar.gz
 sudo bash kasm_release/install.sh -L 8443
