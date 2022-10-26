f2093c0d455c0039db275c086235e3671f2140ce
patinas <andreas.patinas@gmail.com>
2022-10-26 16:04:55 +0200

    Update the1.sh

--- the1.sh
+++ the1.sh
@@ -6,7 +6,7 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install timeshift -y
 sudo apt install curl -y
 
-
+cd ~/Downloads
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb -y
 sudo rm google-chrome-stable_current_amd64.deb
