78c9b1ba4ae47b3552059a0df86b3e4682090713
patinas <andreas.patinas@gmail.com>
2022-11-07 18:53:49 +0000

    Update portainer.sh

--- docker/portainer.sh
+++ docker/portainer.sh
@@ -19,8 +19,14 @@ sudo docker run -d \
 -v portainer_data:/data \
 portainer/portainer-ce:latest
 
-# sudo curl -s https://install.zerotier.com | sudo bash
-# sudo zerotier-cli join 9f77fc393e977c16
+while true; do ping -c1 www.google.com > /dev/null && break; done
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install curl -y
+
+sudo curl -fsSL https://tailscale.com/install.sh | sh
+sudo tailscale up --authkey tskey-auth-kmYWEs4CNTRL-E58dqRPnpjitN4wH1JYshiDnogxNZCqR --unattended
 
 sudo chmod +x portainer.sh
 
