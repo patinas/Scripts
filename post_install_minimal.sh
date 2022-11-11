9682222d60b11911949b15d1c275e14054d58747
patinas <andreas.patinas@gmail.com>
2022-11-11 12:44:49 +0100

    Update post_install_minimal.sh

--- post_install_minimal.sh
+++ post_install_minimal.sh
@@ -10,14 +10,14 @@ sudo apt install docker.io docker-compose -y
 sudo systemctl enable docker
 sudo systemctl start docker
 
-sudo docker run -d \
---name="portainer" \
---restart on-failure \
--p 9000:9000 \
--p 8000:8000 \
--v /var/run/docker.sock:/var/run/docker.sock \
--v portainer_data:/data \
-portainer/portainer-ce:latest
+# sudo docker run -d \
+# --name="portainer" \
+# --restart on-failure \
+# -p 9000:9000 \
+# -p 8000:8000 \
+# -v /var/run/docker.sock:/var/run/docker.sock \
+# -v portainer_data:/data \
+# portainer/portainer-ce:latest
 
 sudo curl -fsSL https://tailscale.com/install.sh | sh
 sudo tailscale up --authkey tskey-auth-kmYWEs4CNTRL-E58dqRPnpjitN4wH1JYshiDnogxNZCqR
@@ -34,13 +34,12 @@ sudo apt install xclip -y
 
 sudo apt update && sudo apt upgrade -y
 
-sudo cp /etc/systemd/logind.conf /etc/systemd/logind.conf.old
-sudo tee -a /etc/systemd/logind.conf <<EOF
-HandleLidSwitch=ignore
-LidSwitchIgnoreInhibited=no
-EOF
-
-sudo service systemd-logind restart
+# sudo cp /etc/systemd/logind.conf /etc/systemd/logind.conf.old
+# sudo tee -a /etc/systemd/logind.conf <<EOF
+# HandleLidSwitch=ignore
+# LidSwitchIgnoreInhibited=no
+# EOF
+# sudo service systemd-logind restart
 
 sudo tailscale ip -4
 
