317706bf0faa7ac6720c55abd24e8611d657bd1f
patinas <andreas.patinas@gmail.com>
2022-11-24 15:20:00 +0100

    test

--- packer.sh
+++ packer.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install packer
 
-sudo apt isntall ansible -y
+wget https://releases.hashicorp.com/packer/1.8.4/packer_1.8.4_linux_amd64.zip
 
