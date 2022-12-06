3fdf14d350966fe95df9151fbcf771f8e56de0c1
patinas <andreas.patinas@gmail.com>
2022-12-06 12:53:29 +0100

    Create kvmdocker.sh

---
+++ kvmdocker.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+while true; do ping -c1 www.google.com > /dev/null && break; done
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install curl -y
+
+sudo apt install docker.io docker-compose -y
+sudo systemctl enable docker
+sudo systemctl start docker
+
+sudo curl -fsSL https://tailscale.com/install.sh | sh
+sudo tailscale up --authkey tskey-auth-kmYWEs4CNTRL-E58dqRPnpjitN4wH1JYshiDnogxNZCqR
+
+curl https://getmic.ro | bash
+sudo chown root:root micro
+sudo mv micro /usr/local/bin
+sudo apt install xclip -y
+
+sudo tee -a /etc/ssh/sshd_config <<EOF
+HostKeyAlgorithms +ssh-rsa
+EOF
+sudo service ssh restart
+
+sudo apt install timeshift -y
+# sudo timeshift --list-devices
+sudo timeshift --create --comments "FRESH" --snapshot-device /dev/dm-0
+# sudo timeshift --restore
+
+sudo echo 'user ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers
+
+sudo tailscale ip -4
+
+
+sudo apt install -y -- libvirt-daemon-system
+sudo apt install git -y
+git clone https://github.com/ms-jpq/windows-in-docker
+
+sudo tee ~/windows-in-docker/docker-compose.yml <<EOF
+---
+version: "3.9"
+
+services:
+  libvirt:
+    restart: always
+    image: msjpq/windows-in-docker:latest
+    security_opt:
+      - apparmor=unconfined
+    environment:
+      TZ:
+      # Make sure DATA_DIR is the same across the three `${DATA_DIR}`s
+      # The windows drivers are also downloaded under here.
+      DATA_DIR: "/kvmdata"
+    ports:
+      - 80:8080
+    volumes:
+      - /var/run/libvirt/libvirt-sock:/var/run/libvirt/libvirt-sock:ro
+      - "/kvmdata:/kvmdata"
+EOF
+
+### For laptop deployment
+# sudo cp /etc/systemd/logind.conf /etc/systemd/logind.conf.old
+# sudo tee -a /etc/systemd/logind.conf <<EOF
+# HandleLidSwitch=ignore
+# LidSwitchIgnoreInhibited=no
+# EOF
+# sudo service systemd-logind restart
