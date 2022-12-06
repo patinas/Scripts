38823e5abd2de7c990dbd5d788fc3678c66e2af1
patinas <andreas.patinas@gmail.com>
2022-12-06 12:54:57 +0100

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -6,11 +6,13 @@ sudo apt update && sudo apt upgrade -y
 
 sudo apt install curl -y
 
+sudo curl -fsSL https://tailscale.com/install.sh | sh
+sudo tailscale up --authkey tskey-auth-kmYWEs4CNTRL-E58dqRPnpjitN4wH1JYshiDnogxNZCqR
+
 sudo curl -s https://install.zerotier.com | sudo bash
 sudo zerotier-cli join 9f77fc393e977c16
 
 sudo apt install xrdp -y
 sudo systemctl start xrdp
 sudo systemctl enable xrdp
-sudo adduser xrdp ssl-cert
-gnome-session-save --force-logout
+sudo reboot
