1cef037c4edb652da9dff6de94f29d29e2275fc3
patinas <andreas.patinas@gmail.com>
2022-12-08 16:58:34 +0100

    Update the1.sh

--- the1.sh
+++ the1.sh
@@ -12,6 +12,8 @@ sudo add-apt-repository ppa:gezakovacs/ppa
 sudo apt-get update
 sudo apt-get install unetbootin -y
 
+sudo apt install gnome-multi-writer
+
 cd ~/Downloads
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb -y
