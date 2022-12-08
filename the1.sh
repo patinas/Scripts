2a61068ba48923c3733571bf1ea7f16c086ef28f
patinas <andreas.patinas@gmail.com>
2022-12-08 16:55:31 +0100

    Update the1.sh

--- the1.sh
+++ the1.sh
@@ -8,6 +8,10 @@ sudo apt install curl -y
 sudo apt install remmina -y
 sudo apt install xrdp -y
 
+sudo add-apt-repository ppa:gezakovacs/ppa
+sudo apt-get update
+sudo apt-get install unetbootin -y
+
 cd ~/Downloads
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb -y
