83771181fb3780cc0306bd4d02ba5a232ec5e776
patinas <andreas.patinas@gmail.com>
2022-12-16 20:13:08 +0100

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -4,12 +4,6 @@ while true; do ping -c1 www.google.com > /dev/null && break; done
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install curl -y
-
-sudo apt install docker.io docker-compose -y
-sudo systemctl enable docker
-sudo systemctl start docker
-
 
 sudo echo 'user ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers
 
@@ -22,12 +16,7 @@ sudo passwd root
 snap install ngrok
 ngrok config add-authtoken 2J05ZW80NZQ0Mjg8oK1Wi1YBQwF_2cfNh6VrZCrsU1JEDvk7Y
 screen -d -m ngrok start tcp 22
-screen -r
+echo "screen -r"
 
-
-
-
-
-sudo tailscale ip -4
 echo Done
 
